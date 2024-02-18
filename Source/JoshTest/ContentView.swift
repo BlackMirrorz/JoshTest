1242147798ad982036841ac5925afa01a2e14894
PongoBongoSaurus <35516944+BlackMirrorz@users.noreply.github.com>
2024-02-18 17:25:41 +0900

    Initial Commit

---
+++ Source/JoshTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JoshTest
+//
+//  Created by Josh Robbins on 2/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Source/JoshTest/JoshTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshTestApp.swift
+//  JoshTest
+//
+//  Created by Josh Robbins on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct JoshTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
